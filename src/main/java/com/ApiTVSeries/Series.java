53af0d2db621f8ba8bf1500e9cbcd4d60a0e03a8
Theo Dirk <theodirk17@gmail.com>
2023-03-08 19:08:19 -0300

    atualizacao do dia tres

--- src/main/java/com/ApiTVSeries/Series.java
+++ src/main/java/com/ApiTVSeries/Series.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class Series {
 
