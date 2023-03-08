38b2cce2433197ed5978f6d611339003f5ef4650
Theo Dirk <theodirk17@gmail.com>
2023-03-08 18:55:01 -0300

    atualizacao do dia tres

--- src/main/java/com/ApiTVSeries/Main.java
+++ src/main/java/com/ApiTVSeries/Main.java
@@ -5,71 +5,43 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 class Main {
+    private static final String imdbAPI = "https://imdb-api.com/en/API/Top250Movies/";
+    private static final String aPIKey = ("k_48519ide");
 
     public static void main(String[] args) throws Exception {
-
-        String apiKey = "k_48519ide";
-        URI apiIMDB = URI.create("https://imdb-api.com/en/API/Top250TVs/" + apiKey);
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(apiIMDB).build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String json = response.body();
-
-        String[] seriesArray = parseJsonSeries(json);
-
-        List<String> titles = parseTitles(seriesArray);
-        titles.forEach(System.out::println);
-
-        List<String> ratting = parseRatting(seriesArray);
-        ratting.forEach(System.out::println);
-
-        List<String> years = parseYears(seriesArray);
-        years.forEach(System.out::println);
-    }
-
-
-
-    private static String[] parseJsonSeries(String json) {
-        Matcher matcher = Pattern.compile(".*\\[(.*)\\].*").matcher(json);
-
-        if (!matcher.matches()) {
-            throw new IllegalArgumentException("no match in " + json);
+        if (aPIKey == null || aPIKey.isEmpty()) {
+            throw new IllegalStateException("Chave está vazia");
         }
+        final var request = HttpRequest.newBuilder()
+                .uri(new URI(imdbAPI + aPIKey))
+                .GET()
+                .build();
 
-        String[] seriesArray = matcher.group(1).split("\\},\\{");
-        seriesArray[0] = seriesArray[0].substring(1);
-        int last = seriesArray.length - 1;
-        String lastString = seriesArray[last];
-        seriesArray[last] = lastString.substring(0, lastString.length() - 1);
-        return seriesArray;
-    }
-
-    private static List<String> parseTitles(String[] seriesArray) {
-        return parseAttribute(seriesArray, 3);
-    }
-
-    private static List<String> parseRatting(String[] seriesArray) {
-        return parseAttribute(seriesArray, 7);
-    }
-
-    private static List<String> parseYears(String[] seriesArray) {
-        return parseAttribute(seriesArray, 4);
-    }
+        final var httpClient = HttpClient.newHttpClient();
+        final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
 
-    private static List<String> parseAttribute(String[] seriesArray, int pos) {
-        return Stream.of(seriesArray)
-                .map(e -> e.split("\",\"")[pos])
-                .map(e -> e.split(":\"")[1])
-                .map(e -> e.replaceAll("\"", ""))
+        final var seriesJson = parseJsonSeries(response.body());
+        final var series = seriesJson.stream()
+                .map(Series::instanceOf)
                 .collect(Collectors.toList());
+        System.out.println(series);
+
     }
+    private static List<String> parseJsonSeries(String json) {
+        final var contentMatcher = Pattern.compile("\\[(.*?)\\]").matcher(json);
+        if (!contentMatcher.find()) {
+            throw new IllegalStateException("Data on wrong format");
+        }
+        var content = contentMatcher.group(0);
+        final var moviesMatcher = Pattern.compile("\\{(.*?)\\}").matcher(content);
+        var groupIndex = 0;
+        return moviesMatcher.results().map(result -> result.group()).collect(Collectors.toList());
+    }
+
+
 }
